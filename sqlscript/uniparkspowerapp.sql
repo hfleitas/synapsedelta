f53dc40b0a52941840927fbc7f219a0beba072ac
Hiram Fleitas <hiramfleitas@hotmail.com>
2021-10-05 19:56:42 -0400

    added demo app url

--- sqlscript/uniparkspowerapp.sql
+++ sqlscript/uniparkspowerapp.sql
@@ -26,3 +26,7 @@ MultipleActiveResultSets=False;
 Encrypt=True;
 TrustServerCertificate=False;
 Connection Timeout=30;
+
+admin@CRM563881.onmicrosoft.com
+https://make.preview.powerapps.com/e/Default-ddb72b0c-6ba3-4e40-8d34-ce9c6717bc94/canvas/?utm_source=office&utm_medium=app_launcher&utm_campaign=office_referrals&action=edit&app-id=%2Fproviders%2FMicrosoft.PowerApps%2Fapps%2F099a4cd3-0dad-475d-b5f2-7725b503724e
+--fBcpKu134T
