37ca53ba5bc145202855a770452a98db51a940de
Hiram Fleitas <hiramfleitas@hotmail.com>
2021-10-05 19:57:13 -0400

    added demo app url

--- sqlscript/uniparkspowerapp.sql
+++ sqlscript/uniparkspowerapp.sql
@@ -17,6 +17,7 @@ exec sp_droprolemember @rolename = 'dbmanager', @membername = 'uniparkspowerapp'
 exec sp_addrolemember @rolename = 'db_owner', @membername = 'uniparkspowerapp';
 
 
+/*
 Server=tcp:hfpocws1.sql.azuresynapse.net,1433;
 Initial Catalog=hfpocsqlpool1;
 Persist Security Info=False;
@@ -30,3 +31,4 @@ Connection Timeout=30;
 admin@CRM563881.onmicrosoft.com
 https://make.preview.powerapps.com/e/Default-ddb72b0c-6ba3-4e40-8d34-ce9c6717bc94/canvas/?utm_source=office&utm_medium=app_launcher&utm_campaign=office_referrals&action=edit&app-id=%2Fproviders%2FMicrosoft.PowerApps%2Fapps%2F099a4cd3-0dad-475d-b5f2-7725b503724e
 --fBcpKu134T
+*/
