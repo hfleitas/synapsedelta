3963795aa97be94cec520fc953e165be5e2bfb80
Hiram Fleitas <hiramfleitas@hotmail.com>
2021-11-18 12:04:42 -0500

    fixed MegreLatest

--- wplussynapsedw/dbo/StoredProcedures/MergeLatest.sql
+++ wplussynapsedw/dbo/StoredProcedures/MergeLatest.sql
@@ -1,15 +1,16 @@
 Create PROC [dbo].[MegreLatest] 
-AS 
-
+as  
 
 merge into [ReportTarget] as t
-    using (select *
-from [lastchanges]) as lc ([key], [time], [newvalue], [deleted])
+    using ( select * 
+            from [lastchanges]
+          ) as lc ([key], [time], [newvalue], [deleted])
     on lc.[key] = t.[key]
     when matched and lc.[deleted] = 1 
         then delete
     when matched
-        then update set t.[key] = lc.[key], t.[value] = lc.[newValue]
+        then update set t.[key] = lc.[key], 
+                        t.[value] = lc.[newValue]
     when not matched and lc.[deleted] = 0 
         then insert ([key], [value]) values(lc.[key], lc.[newValue]);
 GO
